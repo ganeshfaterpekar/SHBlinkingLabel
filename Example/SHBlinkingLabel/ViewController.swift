67c329a83e1505094e325a82e25cde118901cf10
ganesh faterpekar <ganeshfaterpekar@gmail.com>
2019-06-27 12:29:53 +1000

    Basic setup

--- Example/SHBlinkingLabel/ViewController.swift
+++ Example/SHBlinkingLabel/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import SHBlinkingLabel
+
 
 class ViewController: UIViewController {
 
+    var isBlinking = false
+    let blinkingLabel = SHBlinkingLabel(frame: CGRect(x: 10,y: 20,width: 200,height: 30))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup the BlinkingLabel
+        blinkingLabel.text = "I blink!"
+        blinkingLabel.font = UIFont.systemFont(ofSize: 20)
+        view.addSubview(blinkingLabel)
+        blinkingLabel.startBlinking()
+        isBlinking = true
+        
+        // Create a UIButton to toggle the blinking
+        let toggleButton = UIButton(frame: CGRect(x: 10,y: 20,width: 200,height: 30))
+        toggleButton.setTitle("Toggle Blinking", for: .normal)
+        toggleButton.setTitleColor(UIColor.red, for: .normal)
+        toggleButton.addTarget(self, action: Selector("toggleBlinking"), for: .touchUpInside)
+        view.addSubview(toggleButton)
+        
     }
 
     override func didReceiveMemoryWarning() {
