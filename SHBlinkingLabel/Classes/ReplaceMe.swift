99d1705e660dc21e850bbdc19c4b58154878a746
ganesh faterpekar <ganeshfaterpekar@gmail.com>
2019-06-27 12:58:42 +1000

    Relinking files

---
+++ SHBlinkingLabel/Classes/ReplaceMe.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+
+
+public class SHBlinkingLabel : UILabel {
+    public func startBlinking() {
+        let options : UIView.AnimationOptions = [.repeat,.autoreverse]
+        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+        }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
